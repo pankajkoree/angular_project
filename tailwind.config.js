067b9aa9680cb66789cb238ad2831ca6205db693
pankajkoree <pankajkori2057@gmail.com>
2024-12-20 13:41:54 +0530

    configure tailind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{html,ts}',  // Ensure Angular components and templates are included
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
